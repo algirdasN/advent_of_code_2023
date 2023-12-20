b64a5a76a8e1b5ff38938aa7af1794d735478273
Algirdas Neniskis <61432976+algirdasN@users.noreply.github.com>
2023-12-20 18:51:24 +0200

    Problem 18 - part 2

--- 18.py
+++ 18.py
@@ -1,48 +1,104 @@
 from collections import defaultdict
 
 
-def main():
-    with open("data/18.txt") as file:
-        instructions = file.read().split("\n")
+def consolidate_ranges(ranges):
+    range_copy = [x for x in ranges]
+    ranges.clear()
 
+    count = 0
+    while range_copy:
+        r = range_copy.pop(0)
+        for i in range(len(range_copy)):
+            if range_copy[i].stop == r.start + 1 or range_copy[i].start + 1 == r.stop:
+                count += 1
+                other = range_copy.pop(i)
+                range_copy.append(range(
+                    min(other.start, r.start),
+                    max(other.stop, r.stop)
+                ))
+                break
+        else:
+            ranges.append(r)
+
+    return count
+
+
+def get_area(directions):
     dir_map = {
         "R": (0, 1),
         "D": (1, 0),
         "L": (0, -1),
         "U": (-1, 0)
     }
-    directions = [x.split(" ")[:2] for x in instructions]
-    visited = defaultdict(set)
 
     curr = (0, 0)
-    visited[curr[0]].add(curr[1])
+    nodes = defaultdict(set)
+    nodes[curr[0]].add(curr[1])
+
     for d in directions:
-        for i in range(int(d[1])):
-            curr = (curr[0] + dir_map[d[0]][0], curr[1] + dir_map[d[0]][1])
-            visited[curr[0]].add(curr[1])
+        curr = (curr[0] + int(d[1]) * dir_map[d[0]][0], curr[1] + int(d[1]) * dir_map[d[0]][1])
+        nodes[curr[0]].add(curr[1])
 
-    filled = defaultdict(set)
-    for k in sorted(visited):
-        s_v = sorted(visited[k])
-        start = s_v[0]
-        inner = True
-        for i in range(1, len(s_v)):
-            if s_v[i] == s_v[i - 1] + 1:
-                continue
+    s_keys = sorted(nodes)
+    s_values = sorted(nodes[s_keys[0]])
+    active_ranges = [range(s_values[i], s_values[i + 1] + 1) for i in range(0, len(s_values), 2)]
+    total = sum(len(x) for x in active_ranges)
 
-            if s_v[i - 1] != start:
-                filled[k].update(range(start, s_v[i - 1] + 1))
-                inner = all(x in filled[k - 1] for x in range(s_v[i - 1], s_v[i]))
+    for i in range(1, len(s_keys)):
+        total += (s_keys[i] - s_keys[i - 1]) * sum(len(x) for x in active_ranges)
+        s_values = sorted(nodes[s_keys[i]])
 
-            if inner:
-                filled[k].update(range(s_v[i - 1], s_v[i] + 1))
-            inner = not inner
-            start = s_v[i]
+        for j in range(0, len(s_values), 2):
+            for k in range(len(active_ranges)):
+                if s_values[j] == active_ranges[k].start and s_values[j + 1] == active_ranges[k].stop - 1:
+                    active_ranges.pop(k)
+                    break
 
-        if start != s_v[-1]:
-            filled[k].update(range(start, s_v[-1] + 1))
+                if s_values[j] == active_ranges[k].start:
+                    active_ranges[k] = range(s_values[j + 1], active_ranges[k].stop)
+                    break
 
-    print(sum(len(x) for x in filled.values()))
+                if s_values[j + 1] == active_ranges[k].stop - 1:
+                    active_ranges[k] = range(active_ranges[k].start, s_values[j] + 1)
+                    break
+
+                if s_values[j + 1] == active_ranges[k].start or s_values[j] == active_ranges[k].stop - 1:
+                    active_ranges[k] = range(
+                        min(s_values[j], active_ranges[k].start),
+                        max(s_values[j + 1] + 1, active_ranges[k].stop)
+                    )
+                    total += s_values[j + 1] - s_values[j]
+                    break
+
+                if s_values[j] > active_ranges[k].start and s_values[j + 1] < active_ranges[k].stop:
+                    old = active_ranges.pop(k)
+                    active_ranges.append(range(old.start, s_values[j] + 1))
+                    active_ranges.append(range(s_values[j + 1], old.stop))
+                    break
+
+            else:
+                active_ranges.append(range(s_values[j], s_values[j + 1] + 1))
+                total += s_values[j + 1] - s_values[j] + 1
+
+        total -= consolidate_ranges(active_ranges)
+
+    return total
+
+
+def main():
+    with open("data/18.txt") as file:
+        instructions = file.read().split("\n")
+
+    directions = [x.split(" ")[:2] for x in instructions]
+
+    print(get_area(directions))
+
+    directions = []
+    for i in instructions:
+        x = i.split(" ")[2]
+        directions.append(("RDLU"[int(x[-2])], int(x[2:-2], 16)))
+
+    print(get_area(directions))
 
 
 if __name__ == "__main__":
